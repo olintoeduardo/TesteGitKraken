003c8485b38623392e2b13a5a152a8f9f289515a
Eduardo Olinto <duolinto@gmail.com>
2022-10-11 15:19:18 -0300

    Initial Commit

---
+++ TesteGitKraken/TesteGitKrakenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteGitKrakenApp.swift
+//  TesteGitKraken
+//
+//  Created by Eduardo Olinto on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TesteGitKrakenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
